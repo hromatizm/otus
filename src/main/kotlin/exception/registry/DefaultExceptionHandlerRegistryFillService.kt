8021692f472a38501e428884895aebcbdd33917b
Михалищин Дмитрий Викторович <hromatizm@gmail.com>
2025-02-09 02:19:49 +0300

    DefaultExceptionHandlerRegistryFillService fix

--- src/main/kotlin/exception/registry/DefaultExceptionHandlerRegistryFillService.kt
+++ src/main/kotlin/exception/registry/DefaultExceptionHandlerRegistryFillService.kt
@@ -2,10 +2,11 @@ package exception.registry
 
 import exception.command.ICommandFlow
 import exception.command.RetryOnceCommand
+import exception.command.RetryTwiceCommand
+import exception.handler.DefaultExceptionHandler
 import exception.handler.RetryOnceExceptionHandler
 import exeption.handler.RetryTwiceExceptionHandler
 import motion.move.Move
-import org.example.PrintACommand
 import java.io.IOException
 import java.sql.SQLException
 
@@ -32,19 +33,9 @@ class DefaultExceptionHandlerRegistryFillService(
                 handler = RetryTwiceExceptionHandler(commandFlow = commandFlow)
             )
             registerHandler(
-                sourceClass = RetryOnceCommand::class.java,
-                exc = IOException::class.java,
-                handler = RetryTwiceExceptionHandler(commandFlow = commandFlow)
-            )
-            registerHandler(
-                sourceClass = PrintACommand::class.java,
+                sourceClass = RetryTwiceCommand::class.java,
                 exc = RuntimeException::class.java,
-                handler = RetryOnceExceptionHandler(commandFlow = commandFlow)
-            )
-            registerHandler(
-                sourceClass = RetryOnceCommand::class.java,
-                exc = RuntimeException::class.java,
-                handler = RetryTwiceExceptionHandler(commandFlow = commandFlow)
+                handler = DefaultExceptionHandler(commandFlow = commandFlow)
             )
         }
     }
