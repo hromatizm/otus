1d8abcd060e5a7da65e0917f96c0b4583fff874d
Михалищин Дмитрий Викторович <hromatizm@gmail.com>
2025-04-23 02:12:31 +0300

    minor fix

--- src/test/kotlin/spring/MsgControllerTest.kt
+++ src/test/kotlin/spring/MsgControllerTest.kt
@@ -7,9 +7,7 @@ import ioc.Ioc
 import motion.Vector
 import org.assertj.core.api.Assertions
 import org.awaitility.Awaitility
-import spring.registry.GameObjRegistry
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.TestInstance
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
